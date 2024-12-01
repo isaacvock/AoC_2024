29ffadbec95bc9fda74006de8d5ce4f7eb5ef75c
isaacvock <isaac.vock@gmail.com>
2024-12-01 12:18:37 -0500

    Implement and benchmark several other R solutions

--- day_1/solution.R
+++ day_1/solution.R
@@ -5,6 +5,8 @@
 
 library(data.table)
 library(bench)
+library(dplyr)
+library(readr)
 
 ### Solution as a function
 solve_day1 <- function(){
@@ -26,18 +28,111 @@ solve_day1 <- function(){
   # Calculate distance
   distance <- sum(abs(V2_sorted - V1_sorted))
 
+  distance
+
   ### Part 2: Calculate simularity score
 
-  left_values <- unique(input$V1)
+  right_counts <- input[,.N, by = V2]
+  colnames(right_counts) <- c("V1", "N")
 
-  similarity <- sum(sapply(left_values,
-                           function(x){
-                             sum(input$V2 == x)*x
-                           }))
+  setkey(right_counts, V1)
+  setkey(input, V1)
+
+
+  similarity <- input[right_counts, nomatch = NULL]
+
+  similarity_score <- similarity[,.(score = sum(V1*N))]
+
+  return(
+    list(similarity = similarity_score,
+         distance = distance)
+  )
 
 }
 
-# Solve ------------------------------------------------------------------------
+
+### base R version as function
+baseR_solution <- function(){
+
+  input <- read.table("C:/Users/isaac/Documents/AoC/2024/day_1/input.txt",
+                      header = FALSE)
+
+  left_values <- sort(input[,1])
+  right_values <- sort(input[,2])
+
+  difference <- sum(abs(left_values - right_values))
+
+  left_values <- unique(input[,1])
+
+  similarity <- sum(sapply(left_values,
+                           function(x){
+                             sum(input[,2] == x)*x
+                           }))
+
+  return(
+    list(similarity = similarity,
+         distance = distance)
+  )
+
+}
+
+
+### tidyverse solution as functoin
+tidyverse_solution <- function(){
+
+  # input <- read_delim("C:/Users/isaac/Documents/AoC/2024/day_1/input.txt",
+  #                     col_names = FALSE,
+  #                     trim_ws = TRUE) %>%
+  #   dplyr::select(-X2, -X3)
+  input <- fread("C:/Users/isaac/Documents/AoC/2024/day_1/input.txt") %>%
+    as_tibble() %>%
+    dplyr::rename(
+      X1 = V1,
+      X4 = V2
+    )
+
+
+  left_values <- input %>%
+    dplyr::arrange(X1) %>%
+    dplyr::select(X1) %>%
+    unlist() %>%
+    unname()
+
+
+  right_values <- input %>%
+    dplyr::arrange(X4) %>%
+    dplyr::select(X4) %>%
+    unlist() %>%
+    unname()
+
+
+  difference <- sum(abs(left_values - right_values))
+
+  right_counts <- input %>%
+    dplyr::count(X4) %>%
+    dplyr::rename(
+      X1 = X4
+    )
+
+  similarity_score <- input %>%
+    dplyr::inner_join(right_counts,
+                      by = "X1") %>%
+    dplyr::summarise(
+      score = sum(X1*n)
+    ) %>%
+    dplyr::select(score) %>%
+    unlist() %>%
+    unname()
+
+
+  return(
+    list(similarity = similarity_score,
+         distance = distance)
+  )
+
+}
+
+# Solve with data.table --------------------------------------------------------
 
 ### Part 1: calculate distance between lists
 
@@ -60,19 +155,88 @@ distance
 
 ### Part 2: Calculate simularity score
 
-left_values <- unique(input$V1)
+right_counts <- input[,.N, by = V2]
+colnames(right_counts) <- c("V1", "N")
+
+setkey(right_counts, V1)
+setkey(input, V1)
+
+
+similarity <- input[right_counts, nomatch = NULL]
+
+similarity_score <- similarity[,.(score = sum(V1*N))]
+similarity_score
+
+
+# Solve with base R only -------------------------------------------------------
+
+input <- read.table("C:/Users/isaac/Documents/AoC/2024/day_1/input.txt",
+                    header = FALSE)
+
+left_values <- sort(input[,1])
+right_values <- sort(input[,2])
+
+difference <- sum(abs(left_values - right_values))
+
+left_values <- unique(input[,1])
 
 similarity <- sum(sapply(left_values,
-       function(x){
-         sum(input$V2 == x)*x
-       }))
+                         function(x){
+                           sum(input[,2] == x)*x
+                         }))
 
-similarity
+
+# Solve with tidyverse ---------------------------------------------------------
+
+input <- read_delim("C:/Users/isaac/Documents/AoC/2024/day_1/input.txt",
+                  col_names = FALSE,
+                  trim_ws = TRUE) %>%
+  dplyr::select(-X2, -X3)
+
+
+left_values <- input %>%
+  dplyr::arrange(X1) %>%
+  dplyr::select(X1) %>%
+  unlist() %>%
+  unname()
+
+
+right_values <- input %>%
+  dplyr::arrange(X4) %>%
+  dplyr::select(X4) %>%
+  unlist() %>%
+  unname()
+
+
+difference <- sum(abs(left_values - right_values))
+
+right_counts <- input %>%
+  dplyr::count(X4) %>%
+  dplyr::rename(
+    X1 = X4
+  )
+
+similarity_score <- input %>%
+  dplyr::inner_join(right_counts,
+                    by = "X1") %>%
+  dplyr::summarise(
+    score = sum(X1*n)
+  ) %>%
+  dplyr::select(score) %>%
+  unlist() %>%
+  unname()
+
+
 
 
 # Benchmark --------------------------------------------------------------------
 
-# Around 4 ms to solve
 (result <- bench::mark(
-  solve_day1()
+  solve_day1(),
+  baseR_solution(),
+  tidyverse_solution(),
+  check = FALSE
 ))
+# data.table: 2.6 ms
+# baseR: 8.5 ms
+# tidyverse: 74 ms!! Down to 13.6 if I swap in fread()
