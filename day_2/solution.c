ef8d83996d48387a0a50c706cd29c119fca3c285
isaacvock <isaac.vock@gmail.com>
2024-12-02 20:30:53 -0500

    Tested C implementation of part 1

--- day_2/solution.c
+++ day_2/solution.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define MAX_LEN 256
@@ -23,7 +23,7 @@ int determine_safeness(int *D, int n){
         return 0; // Not safe
     }
 
-    for(int i = 1; i < n; i++){
+    for(int i = 0; i < n; i++){
 
         if((D[i] > 0 && decreasing) || (D[i] < 0 && !decreasing) || (D[i] == 0) || (abs(D[i]) > 3)){
 
@@ -38,7 +38,7 @@ int determine_safeness(int *D, int n){
 }
 
 /* Determine differences between adjacent elements, from which all else is derivable */
-void calc_differences(int **D, const int *a, int n){
+void calc_differences(int **D, int *a, int n){
 
     for(int i = 0; i < (n-1); i++){
         (*D)[i] = a[i + 1] - a[i];
@@ -47,7 +47,7 @@ void calc_differences(int **D, const int *a, int n){
 }
 
 /* For generating array from line */
-void parse_numbers(int **a, const char *L){
+void parse_numbers(int **a, char *L){
 
     int index = 0;
 
@@ -64,9 +64,9 @@ void parse_numbers(int **a, const char *L){
 }
 
 /* For determining how many numbers are in the string */
-int count_spaces(const char *L){
+int count_spaces(char *L){
 
-    int counter;
+    int counter = 0;
 
     for(; *L != '\0'; L++){
 
@@ -85,12 +85,14 @@ int main(int argc, char argv[]){
     /* Open file */
     FILE *fptr;
 
-    fptr = fopen("input.tsv", "r");
+    fptr = fopen("input.txt", "r");
 
     char line[MAX_LEN];
 
     int safe_counter = 0;
-
+    int n_nums;
+    int safety;
+    int line_count = 1;
 
     /* Parse line by line */
     if(fptr != NULL){
@@ -98,7 +100,9 @@ int main(int argc, char argv[]){
         while (fgets(line, MAX_LEN, fptr)){
 
             // Figure out how many numbers are in the line
-            int n_nums = count_spaces(line) + 1;
+            n_nums = count_spaces(line) + 1;
+
+            // printf("Number of numbers is %d\n", n_nums);
 
             // If only one number, it's definitely safe
             if(n_nums == 1){
@@ -109,12 +113,31 @@ int main(int argc, char argv[]){
             int *a = malloc(n_nums * sizeof(int));
             parse_numbers(&a, line);
 
+            /* DEBUGGING */
+            // printf("\nNUMBER ARRAY :\n");
+            // for(int i = 0; i<n_nums; i++){
+            //     printf("%d ", a[i]);
+            // }
+
             // Determine difference between each element
             int *diffs = malloc((n_nums - 1) * sizeof(int));
             calc_differences(&diffs, a, n_nums);
 
+            /* DEBUGGING */
+            // printf("\nDIFFERENCE ARRAY:\n");
+            // for(int i = 0; i<(n_nums-1); i++){
+            //     printf("%d ", diffs[i]);
+            // }
+
             // Determine if it is safe
-            safe_counter += determine_safeness(diffs, n_nums - 1);
+            safety = determine_safeness(diffs, n_nums - 1);
+
+            // if(safety == 1){
+            //     printf("Line %d is safe");
+            // }
+
+            safe_counter += safety;
+            line_count++;
 
             free(a);
 
